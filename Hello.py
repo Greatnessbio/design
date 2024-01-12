0bb442ba3162ed165176738eb2d4e6348fe23908
Greatnessbio <154548997+Greatnessbio@users.noreply.github.com>
2024-01-12 14:47:40 -0500

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,51 +1,48 @@
-# Copyright 2018-2022 Streamlit Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
-from streamlit.logger import get_logger
+import ffmpeg
+import os
+from tempfile import NamedTemporaryFile
 
-LOGGER = get_logger(__name__)
-
-
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
+def compress_video(input_path, output_path, bitrate, codec, preset):
+    (
+        ffmpeg
+        .input(input_path)
+        .output(output_path, video_bitrate=bitrate, vcodec=codec, preset=preset, pix_fmt='yuv420p')
+        .run(overwrite_output=True)
     )
 
-    st.write("# Welcome to Streamlit! 👋")
+def main():
+    st.title("Video Compressor")
 
-    st.sidebar.success("Select a demo above.")
+    uploaded_file = st.file_uploader("Upload a video file", type=["mp4", "avi", "mov", "mkv"])
 
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+    if uploaded_file is not None:
+        with NamedTemporaryFile(delete=False, suffix='.' + uploaded_file.name.split('.')[-1]) as temp_file:
+            temp_file.write(uploaded_file.getvalue())
+            input_path = temp_file.name
 
+        st.video(input_path)
+
+        # User input for compression settings
+        bitrate = st.text_input("Bitrate (e.g., 1M, 500k)", value="1M")
+        codec = st.selectbox("Codec", ["libx264", "libx265", "mpeg4"])
+        preset = st.selectbox("Compression Preset", ["ultrafast", "fast", "medium", "slow"])
+
+        if st.button('Compress Video'):
+            output_path = input_path.split('.')[0] + '_compressed.mp4'
+            compress_video(input_path, output_path, bitrate, codec, preset)
+
+            with open(output_path, "rb") as file:
+                btn = st.download_button(
+                        label="Download Compressed Video",
+                        data=file,
+                        file_name="compressed_video.mp4",
+                        mime="video/mp4"
+                    )
+
+            os.remove(output_path)  # Clean up the temporary file
+
+        os.remove(input_path)  # Clean up the input file
 
 if __name__ == "__main__":
-    run()
+    main()
